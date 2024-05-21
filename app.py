8875e70ff05490616fe34a6a69c946a9ab3ea183
Alejandro Gonzalez <greeny090315@gmail.com>
2024-05-20 22:51:10 -0400

    token expirations no longer result in an error

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
 from find_work import scrape_work_schedule
 from create_event import create_event_final
 
-shifts = scrape_work_schedule()
-for shiftKey, shiftValue in shifts.items():
-    start = shiftValue[0]
-    end = shiftValue[1]
-    create_event_final(start,end)
+#if google auth token expiration error, delete the token and run the program again
+try: 
+    shifts = scrape_work_schedule()
+    for shiftKey, shiftValue in shifts.items():
+        start = shiftValue[0]
+        end = shiftValue[1]
+        create_event_final(start,end)
+except:
+    os.remove("./CREDENTIALS/token.json")
+    shifts = scrape_work_schedule()
+    for shiftKey, shiftValue in shifts.items():
+        start = shiftValue[0]
+        end = shiftValue[1]
+        create_event_final(start,end)
